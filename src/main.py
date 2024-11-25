1e87cdead2d0060f3329d83427abe969645018f0
沈王寻 <114404517@qq.com>
2024-11-25 18:41:43 +0800

    测试命令行运行

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print("hello")
+# Simple Fuzzer的入口程序
+
+import argparse
+
+
+
+# 创建解析器
+parser = argparse.ArgumentParser()
+parser.add_argument('-i',type=str,help='种子输入目录')
+parser.add_argument('-o',type=str,help='执行结果输出目录')
+
+args = parser.parse_args()
+
+if args.i is None:
+    args.i = input('请输入种子输入目录：')
+
+if args.o is None:
+    args.o = input('请输入执行结果输出目录：')
+
+print(args.i)
+print(args.o)
